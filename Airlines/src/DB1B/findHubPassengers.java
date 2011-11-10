61098b26bbb820b9e4bc0ba159b4e4baa26f479d
andi <lindenblatt@uni-hd.de>
2011-11-10 17:03:11 +0100

    new class initialized

---
+++ Airlines/src/DB1B/findHubPassengers.java
@@ -0,0 +1,21 @@
+package DB1B;
+
+public class findHubPassengers {
+	
+	/**
+	 * TODO:
+	 * 
+	 * Read in airportGroup and passengers
+	 * Skip first two tokens
+	 * If there still is a next token left, check if airport is already in THashMap(key=airport, value=passengers)
+	 * Add passengers
+	 * print Hashmap in csv 
+	 * 
+	 */
+	
+	
+	
+
+}
+
+
